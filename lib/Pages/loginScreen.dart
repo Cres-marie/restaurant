b129a58271db1252195ab3079fb6dd7034111486
mariecrescentia1 <mariecrescentia00@gmail.com>
2023-09-18 20:20:56 +0300

    added

---
+++ lib/Pages/loginScreen.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant/components/Animations/fadeAnimation.dart';
+import 'package:restaurant/components/Animations/sildeAnimation.dart';
+import 'package:restaurant/constants/constants.dart';
+
+
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+
+    // _tapGestureRecognizer = TapGestureRecognizer()
+    //   ..onTap = () {
+    //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => WRSingup1()));
+    //   };
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+
+          // Add the top clipper
+          ClipPath(
+            clipper: TopClipper(), // Use the custom clipper
+            child: Container(
+              height: 200,
+              color: buttonColor, // Customize the color
+            ),
+          ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              SizedBox(
+                height: 250,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:[
+                    SlideAnimation(
+                      position: 1,
+                      itemCount: 8,
+                      slideDirection: SlideDirection.fromLeft,
+                      animationController: _animationController,
+                      child: Text(
+                        "Hello There, \nWelcome Back",
+                        style: h1
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    SlideAnimation(
+                      position: 1,
+                      itemCount: 8,
+                      slideDirection: SlideDirection.fromLeft,
+                      animationController: _animationController,
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.transparent,
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.grey[100]!,
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Username",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.grey[100]!,
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(color: Colors.grey)),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SlideAnimation(
+                            position: 1,
+                            itemCount: 8,
+                            slideDirection: SlideDirection.fromLeft,
+                            animationController: _animationController,
+                            child: Text(
+                              "Forgot Password?",
+                              style: links
+                            ),
+                          
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    SlideAnimation(
+                      position: 1,
+                      itemCount: 8,
+                      slideDirection: SlideDirection.fromLeft,
+                      animationController: _animationController,
+                      child: Container(
+                        height: 50,
+                        margin: EdgeInsets.symmetric(horizontal: 60),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: buttonColor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: buttonText
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 60.0,
+                    ),
+                    
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SlideAnimation(
+                          position: 1,
+                          itemCount: 8,
+                          slideDirection: SlideDirection.fromLeft,
+                          animationController: _animationController,
+                          child: Center(
+                            child: Text(
+                              "Dont have an account?",
+                              // style: TextStyle(
+                              //   color: Colors.pink[200],
+                              // ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5,),
+                        SlideAnimation(
+                          position: 1,
+                          itemCount: 8,
+                          slideDirection: SlideDirection.fromLeft,
+                          animationController: _animationController,
+                          child: Center(
+                            child: Text(
+                              "Sign Up",
+                              style: links
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  FadeAnimation(this.delay, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("opacity").add(
+          Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0),
+          curve: Curves.bounceIn),
+      Track("translateY").add(
+          Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
+          curve: Curves.easeOut)
+    ]);
+
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (500 * delay).round()),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, dynamic animation) => Opacity(
+        opacity: animation["opacity"],
+        child: Transform.translate(
+            offset: Offset(0, animation["translateY"]), child: child),
+      ),
+      animationControllerStatusListener: (AnimationStatus status) {},
+      builder: (BuildContext buildContext, Map<String, dynamic> animatedValue) {
+        return SizedBox();
+      },
+    );
+  }
+}
+
+
+
+class TopClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height * 0.8); // Start from the bottom-left corner
+    // Define a curve for the top edge
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height * 0.8);
+    path.lineTo(size.width, 0); // Finish at the bottom-right corner
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false; // We don't need to update the clipper
+  }
+}
---
+++ lib/constants/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const h1 = TextStyle(
+  fontSize: 35,
+  fontFamily: "Sofia",
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+const links = TextStyle(fontFamily: "Sofia", color: Color(0xFF047CE3), fontWeight: FontWeight.w600);
+
+const buttonColor = Color(0xFF047CE3);
+
+const buttonText = TextStyle(
+  fontWeight: FontWeight.w700,
+  color: Colors.white,
+  fontSize: 18.0,
+  fontFamily: "Sofia",
+);
